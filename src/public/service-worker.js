9b08ab3b866c826c6ba1ec2fadb19289a1202004
varshik-obbe <mktg@obbe.in>
2023-03-22 17:58:44 +0530

    updated changes for pdf alignment for crm

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.759e8982ab6ba2ff0c8c78c6026f3463.js"
+  "/precache-manifest.63604fa727faf212ff9241db89e1eefa.js"
 );
 
 self.addEventListener('message', (event) => {
