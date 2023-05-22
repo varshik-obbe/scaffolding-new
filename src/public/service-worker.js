476e4f87728e307ade6928379695471e813873c6
varshik-obbe <mktg@obbe.in>
2023-05-22 11:28:54 +0530

    updated the quotation and invoice to have 12 items

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fc337d9074906da06de3d10be10057f6.js"
+  "/precache-manifest.e14a332a82fa162e040503e20356dcd6.js"
 );
 
 self.addEventListener('message', (event) => {
